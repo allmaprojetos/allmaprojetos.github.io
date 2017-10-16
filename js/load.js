a74c184086e9caca08e49449077569c127572332
allmaprojetos <32419208+allmaprojetos@users.noreply.github.com>
2017-10-16 21:36:28 -0200

    commit

--- js/load.js
+++ js/load.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     //Quem somos nós
-    $.get("https://allmaprojetos.github.io/content/descricao/descricao.html", function(data, status){
-        $('#descricao').html(data);
+    $.get("https://allmaprojetos.github.io/content/descricao/descricao-col-1.html", function(data, status){
+        $('#descricao-col-1').html(data);
     });
+    $.get("https://allmaprojetos.github.io/content/descricao/descricao-col-2.html", function(data, status){
+        $('#descricao-col-2').html(data);
+    });
+    
 });
